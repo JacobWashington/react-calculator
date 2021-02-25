986feb9f9df083705f22ad71cb0e8644a6e7c234
jacobwashington <jacobwashington.dev@gmail.com>
2021-02-25 09:31:08 -0800

    refactor inputDisplay

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import Number from './Number/Number';
-import Operator from './Operator/Operator';
-import './App.css';
+import React, { Component } from "react";
+import Number from "./Number/Number";
+import Operator from "./Operator/Operator";
+import "./App.css";
 
 class App extends Component {
   state = {
     numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-    operators: ['+', '-', '*', '/', '='],
-    firstArgument: '',
-    secondArgument: '',
-    operator: '',
+    operators: ["+", "-", "*", "/", "="],
+    firstArgument: "",
+    secondArgument: "",
+    operator: "",
     result: null,
-  }
+  };
 
   clickNumberHandler = (event) => {
     let selectedNum = event.target.firstChild.textContent;
@@ -26,9 +26,8 @@ class App extends Component {
         return {
           ...prevState,
           secondArgument: updatedInput,
-        }
-
-      })
+        };
+      });
     } else {
       let input = this.state.firstArgument;
       let updatedInput = input.concat(selectedNum);
@@ -36,92 +35,116 @@ class App extends Component {
       this.setState((prevState) => {
         return {
           ...prevState,
-          firstArgument: updatedInput
-        }
-
-      })
+          firstArgument: updatedInput,
+        };
+      });
     }
-  }
+  };
 
   clickOperatorHandler = (event) => {
     let selectedOp = event.target.firstChild.textContent;
-    let equationArr = [`${this.state.firstArgument}`, `${this.state.operator}`, `${this.state.secondArgument}`];
+    let equationArr = [
+      `${this.state.firstArgument}`,
+      `${this.state.operator}`,
+      `${this.state.secondArgument}`,
+    ];
     const evalArr = eval(equationArr.join(""));
 
-    if (selectedOp === '=') {
+    if (selectedOp === "=") {
       this.setState((prevState) => {
         return {
           ...prevState,
           firstArgument: evalArr,
-          secondArgument: '',
-          operator: '',
-          result: evalArr
-        }
-      })
+          secondArgument: "",
+          operator: "",
+          result: evalArr,
+        };
+      });
     } else
       this.setState((prevState) => {
         return {
           ...prevState,
-          operator: selectedOp
-        }
-      })
-  }
+          operator: selectedOp,
+        };
+      });
+  };
 
   inputChangeHandler = (event) => {
-    let userInput = event.target.value
+    let userInput = event.target.value;
 
     this.setState((prevState) => {
       return {
         ...prevState,
-        firstArgument: userInput
-      }
-    })
-  }
+        firstArgument: userInput,
+      };
+    });
+  };
 
   inputDisplay = () => {
+    let display;
     if (this.state.result) {
-      return this.state.result
-    } else if
-      (this.state.secondArgument && this.state.firstArgument > 0) {
-      return this.state.secondArgument
-    } else return this.state.firstArgument
-  }
+      display = this.state.result;
+    } else if (this.state.secondArgument && this.state.firstArgument > 0) {
+      display = this.state.secondArgument;
+    } else display = this.state.firstArgument;
+    return display;
+  };
 
   clickClearHandler = () => {
     this.setState((prevState) => {
       return {
         ...prevState,
-        firstArgument: '',
-        secondArgument: '',
-        operator: '',
-        result: '',
-      }
-    })
-  }
+        firstArgument: "",
+        secondArgument: "",
+        operator: "",
+        result: "",
+      };
+    });
+  };
 
   buttonStyle = {
-    textAlign: 'center',
-    margin: '2px',
-    cursor: 'pointer'
-  }
+    textAlign: "center",
+    margin: "2px",
+    cursor: "pointer",
+  };
 
   render() {
-
     return (
       <React.Fragment>
-        <div className='App'>
-          <div className='Container'>
+        <div className="App">
+          <div className="Container">
             <div>
-              <input className='Display' type='text' value={this.inputDisplay()} onChange={this.inputChangeHandler}></input>
+              <input
+                className="Display"
+                type="text"
+                value={this.inputDisplay()}
+                onChange={this.inputChangeHandler}
+              ></input>
             </div>
-            <div className='Button'>
+            <div className="Button">
               {this.state.numbers.map((number, index) => {
-                return <Number number={number} key={index} click={this.clickNumberHandler} style={this.buttonStyle} />
+                return (
+                  <Number
+                    number={number}
+                    key={index}
+                    click={this.clickNumberHandler}
+                    style={this.buttonStyle}
+                  />
+                );
               })}
               {this.state.operators.map((operator, index) => {
-                return <Operator operator={operator} key={index} click={this.clickOperatorHandler} style={this.buttonStyle} />
+                return (
+                  <Operator
+                    operator={operator}
+                    key={index}
+                    click={this.clickOperatorHandler}
+                    style={this.buttonStyle}
+                  />
+                );
               })}
-              <button style={this.buttonStyle} onClick={this.clickClearHandler}>C</button>
+              <button style={this.buttonStyle} onClick={this.clickClearHandler}>
+                C
+              </button>
             </div>
           </div>
         </div>
